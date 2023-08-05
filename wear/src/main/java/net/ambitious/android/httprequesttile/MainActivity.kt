9a4243fcc1b18952fb3d99ad6aa3722ad7b4bb1a
bvlion <bvltiggeari@ambitious-i.net>
2023-08-05 11:58:41 +0900

    fix indent

--- wear/src/main/java/net/ambitious/android/httprequesttile/MainActivity.kt
+++ wear/src/main/java/net/ambitious/android/httprequesttile/MainActivity.kt
@@ -27,56 +27,56 @@ import net.ambitious.android.httprequesttile.theme.HttpRequestTileTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val remoteActivityHelper by lazy { RemoteActivityHelper(this) }
+  private val remoteActivityHelper by lazy { RemoteActivityHelper(this) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            WearApp(::startMobileActivity)
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      WearApp(::startMobileActivity)
     }
+  }
 
-    private fun startMobileActivity() {
-        lifecycleScope.launch {
-            try {
-                remoteActivityHelper.startRemoteActivity(
-                    Intent(Intent.ACTION_VIEW)
-                        .addCategory(Intent.CATEGORY_BROWSABLE)
-                        .setData(Uri.parse("httprequesttile://start"))
-                ).await()
-            } catch (e: Exception) {
-                ConfirmationOverlay()
-                    .setType(ConfirmationOverlay.FAILURE_ANIMATION)
-                    .showOn(this@MainActivity)
-            }
-        }
+  private fun startMobileActivity() {
+    lifecycleScope.launch {
+      try {
+        remoteActivityHelper.startRemoteActivity(
+          Intent(Intent.ACTION_VIEW)
+            .addCategory(Intent.CATEGORY_BROWSABLE)
+            .setData(Uri.parse("httprequesttile://start"))
+        ).await()
+      } catch (e: Exception) {
+        ConfirmationOverlay()
+          .setType(ConfirmationOverlay.FAILURE_ANIMATION)
+          .showOn(this@MainActivity)
+      }
     }
+  }
 }
 
 @Composable
 fun WearApp(startMobileActivity: () -> Unit = {}) {
-    HttpRequestTileTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Button(
-                onClick = startMobileActivity,
-                modifier = Modifier.fillMaxSize().padding(16.dp)
-            ) {
-                Text(
-                    "スマートフォンを\n起動する",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-        }
+  HttpRequestTileTheme {
+    Column(
+      modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colors.background),
+      verticalArrangement = Arrangement.Center
+    ) {
+      Button(
+        onClick = startMobileActivity,
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+      ) {
+        Text(
+          "スマートフォンを\n起動する",
+          modifier = Modifier.padding(16.dp)
+        )
+      }
     }
+  }
 }
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp()
+  WearApp()
 }
