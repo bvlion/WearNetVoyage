faa2db19286a7c88a2b017ec241260aed38fd595
bvlion <bvltiggeari@ambitious-i.net>
2023-09-29 12:50:23 +0900

    add splash screen

--- wear/src/main/java/net/ambitious/android/httprequesttile/WearMainActivity.kt
+++ wear/src/main/java/net/ambitious/android/httprequesttile/WearMainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
@@ -24,6 +25,7 @@ import net.ambitious.android.httprequesttile.theme.HttpRequestTileTheme
 
 class WearMainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
     setContent {
       WearApp {
