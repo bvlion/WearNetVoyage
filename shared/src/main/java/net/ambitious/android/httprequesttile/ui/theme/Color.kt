faf81165d5234fd57fe9165fa87fceb197ff6f73
bvlion <bvltiggeari@ambitious-i.net>
2024-02-12 18:29:45 +0900

    change theme color

--- shared/src/main/java/net/ambitious/android/httprequesttile/ui/theme/Color.kt
+++ shared/src/main/java/net/ambitious/android/httprequesttile/ui/theme/Color.kt
@@ -2,21 +2,19 @@ package net.ambitious.android.httprequesttile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primary = Color(0xFFFF5252)
-val primaryVariant = Color(0xFFFF8A80)
-val secondary = Color(0xFFF4081F)
-
+val primary = Color(0xFF0D47A1)
+val primaryVariant = Color(0xFF08306B)
+val secondary = Color(0xFF546E7A)
 val lightBackground = Color(0xFFFFFFFF)
-val lightSurface = Color(0xFFFFFFFF)
-val lightError = Color(0xFFB00020)
-val lightOnPrimary = Color(0xFF000000)
-val lightOnSecondary = Color(0xFF000000)
+val lightSurface = Color(0xFFFAFAFA)
+val lightError = Color(0xFFD32F2F)
+val lightOnPrimary = Color(0xFFFFFFFF)
+val lightOnSecondary = Color(0xFFFFFFFF)
 val lightOnBackground = Color(0xFF000000)
 val lightOnSurface = Color(0xFF000000)
 val lightOnError = Color(0xFFFFFFFF)
-
 val darkBackground = Color(0xFF121212)
-val darkSurface = Color(0xFF121212)
+val darkSurface = Color(0xFF1E1E1E)
 val darkError = Color(0xFFCF6679)
 val darkOnPrimary = Color(0xFFFFFFFF)
 val darkOnSecondary = Color(0xFFFFFFFF)
